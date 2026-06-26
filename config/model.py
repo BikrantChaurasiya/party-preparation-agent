18987093eb13d1bcc5cd7e9b8934c67e62ae843a
BikrantChaurasiya <vikrant1chaurasiya@gmail.com>
2026-06-26 14:09:56 +0530

    Add function to load OpenAI model

--- config/model.py
+++ config/model.py
@@ -1,1 +1,12 @@
+from smolagents import OpenAIModel
 
+
+def load_model(api_key):
+
+    model = OpenAIModel(
+        model_id="gemini-2.5-flash",
+        api_base="https://generativelanguage.googleapis.com/v1beta/openai/",
+        api_key=api_key
+    )
+
+    return model
