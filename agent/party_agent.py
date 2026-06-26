65c1302459bdc78e97e6e13684f2f860c9b9e873
BikrantChaurasiya <vikrant1chaurasiya@gmail.com>
2026-06-26 14:12:17 +0530

    Update party_agent.py

--- agent/party_agent.py
+++ agent/party_agent.py
@@ -1,1 +1,39 @@
+from smolagents import (
+    CodeAgent,
+    DuckDuckGoSearchTool
+)
 
+from config.model import load_model
+
+from tools.menu import suggest_menu
+from tools.dress_code import suggest_dress_code
+from tools.catering import catering_services_tool
+
+
+def create_agent(api_key):
+
+    model = load_model(api_key)
+
+    agent = CodeAgent(
+
+        tools=[
+
+            DuckDuckGoSearchTool(),
+
+            suggest_menu,
+
+            suggest_dress_code,
+
+            catering_services_tool
+
+        ],
+
+        model=model,
+
+        additional_authorized_imports=[
+            "datetime"
+        ]
+
+    )
+
+    return agent
